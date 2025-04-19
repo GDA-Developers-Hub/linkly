ffc1de62425f11153e37ef46386f9dae0169fbba
IsmaelKiprop <kipropismael27@gmail.com>
2025-04-19 16:31:25 +0300

    updated

--- linkly/settings.py
+++ linkly/settings.py
@@ -130,7 +130,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
