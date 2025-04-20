e24a764570ff89cad9e080f2c848f317703a3925
IsmaelKiprop <kipropismael27@gmail.com>
2025-04-20 07:58:54 +0300

    Configure static files for Railway deployment

--- linkly/settings.py
+++ linkly/settings.py
@@ -152,6 +152,9 @@ STATICFILES_STORAGE = 'linkly.storage.CustomWhiteNoiseStorage'
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
