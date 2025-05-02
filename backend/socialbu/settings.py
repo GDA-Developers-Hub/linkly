76eaf7dfe5bef10d21031eafe2e16bec07ebf837
codingwithtezisfun <eantez254@gmail.com>
2025-05-02 09:48:15 +0300

    updates

--- backend/socialbu/settings.py
+++ backend/socialbu/settings.py
@@ -165,11 +165,18 @@ SIMPLE_JWT = {
 CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS', 'False') == 'True'
 CORS_ALLOWED_ORIGINS = [origin for origin in os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,https://linkly-frontend.up.railway.app,https://linkly-production.up.railway.app,https://linkly-gd.web.app,https://linkly-gd.firebaseapp.com').split(',') if origin]
 
+# CSRF_TRUSTED_ORIGINS = [
+#     "https://linkly-production.up.railway.app",
+#     "https://linkly-gd.web.app",
+#     "https://linkly-gd.firebaseapp.com"
+# ]
+
+
 CSRF_TRUSTED_ORIGINS = [
-    "https://linkly-production.up.railway.app",
-    "https://linkly-gd.web.app",
-    "https://linkly-gd.firebaseapp.com"
+    origin for origin in os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") if origin
 ]
+
+
 
 # If you're making API calls using cookies (session auth)
 CORS_ALLOW_CREDENTIALS = True
