978a93f3f39e88dbd9b92cd527422294af2d94a5
IsmaelKiprop <kipropismael27@gmail.com>
2025-05-06 09:56:45 +0300

    working on the jwt tokens

--- backend/services/google-ads/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-npm install express cors dotenv winston google-ads-api google-auth-library
-
-# Install dev dependencies
-npm install --save-dev typescript @types/node @types/express @types/cors @types/winston ts-node nodemon jest ts-jest @types/jest 
