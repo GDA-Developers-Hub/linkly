add8cb3f35a2e59b5c4ce4483414bbeaae68a6f1
Ismael <kipropismael27@gmail.com>
2025-05-21 07:15:17 -0700

    updated

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -189,9 +189,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "railway",
         "USER": "postgres",
-        "PASSWORD": "yxLhzdNnHLEOGwTKiLeGiPkblTJwHzgH",
-        "HOST": "hopper.proxy.rlwy.net",
-        "PORT": "38987",
+        "PASSWORD": "byoberZtausBJYabTlUIGUxMqbRrgRFp",
+        "HOST": "yamanote.proxy.rlwy.net",
+        "PORT": "12485",
     }
 }
 
