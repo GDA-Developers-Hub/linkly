f4bb584b2d32941192fe0107ab9aa4f242ef8475
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2025-05-21 02:24:56 +0100

    Fix user authentication while linking

--- auth/account/adapters.py
+++ auth/account/adapters.py
@@ -1,8 +1,20 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from django.shortcuts import resolve_url
+from rest_framework_simplejwt.tokens import UntypedToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken
 from django.conf import settings
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def pre_social_login(self, request, sociallogin):
+        token = request.GET.get("token")
+        if token:
+            try:
+                validated = JWTAuthentication().get_validated_token(token)
+                user = JWTAuthentication().get_user(validated)
+                if user:
+                    sociallogin.connect(request, user)
+            except InvalidToken:
+                pass
     def get_connect_redirect_url(self, request, socialaccount):
         """
         Called after a social account is linked to an already logged-in user.
@@ -12,3 +24,4 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
         
         # Example: redirect to your frontend with query param
         return f"{settings.LOGIN_REDIRECT_URL}?linked={provider}"
+    
--- frontend/lib/socials-api.ts
+++ frontend/lib/socials-api.ts
@@ -4,11 +4,12 @@ export class AuthAPI {
 
   // Redirect to link a social account
   link(provider: "google" | "facebook" | "linkedin" | "twitter") {
+    const token = localStorage.getItem("linkly_access_token")
     let url;
     if (provider === "linkedin") {
-      url = `${this.backendBase}/accounts/oidc/${provider}/login/?process=connect`;
+      url = `${this.backendBase}/accounts/oidc/${provider}/login/?process=connect&token=${token}`;
     } else {
-      url = `${this.backendBase}/accounts/${provider}/login/?process=connect`;
+      url = `${this.backendBase}/accounts/${provider}/login/?process=connect&token=${token}`;
     }
     window.location.href = url;
   }
